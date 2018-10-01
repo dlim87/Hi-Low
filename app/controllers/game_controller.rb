db6b0dbb2173016521d8fc954353affcbe94522d
Mary San Agustin <marycsanagustin@gmail.com>
2018-09-30 17:25:33 -0700

    Cleaned up some code, visually easier to read

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,24 +1,27 @@
 class GameController < ApplicationController
+
     def reset
-        session[:secretnum]=rand(100)
-        cookies[:tries]=0
-        cookies[:oldguesses]=""
+        session[:secretnum] = rand(100)
+        cookies[:tries] = 0
+        cookies[:oldguesses] = ""
     end
+
     def landing
-        cookies[:username]= params[:username]
+        cookies[:username] = params[:username]
     end
+
     def try
         @guess = params[:guess].to_i
-        cookies[:oldguesses]="#{cookies[:oldguesses]} #{params[:guess]}"
-        secret=session[:secretnum].to_i
-        @message=""
+        cookies[:oldguesses] = "#{cookies[:oldguesses]} #{params[:guess]}"
+        secret = session[:secretnum].to_i
+        @message = ""
         if @guess == secret
             @result = "right!"
-            if cookies[:tries].to_i<5
+            if cookies[:tries].to_i < 5
                 @message = "YOU GET A GOLD MEDAL!"
-            elsif cookies[:tries].to_i<10
+            elsif cookies[:tries].to_i < 10
                 @message = "YOU GET A SILVER MEDAL."
-            elsif cookies[:tries].to_i<20
+            elsif cookies[:tries].to_i < 20
                 @message = "YOU GET A BRONZE MEDAL :("
             else
                 @message= ""
@@ -30,16 +33,16 @@ class GameController < ApplicationController
         else
             @result = "something went wrong"
         end
-        cookies[:tries]=cookies[:tries].to_i+1
-        if cookies[:tries].to_i ==1
-            @proximity==""
-        elsif (cookies[:previousguess].to_i-secret).abs <=(@guess-secret).abs
-                @proximity="Getting colder..."
-        elsif @guess==secret
-                @proximity=""
+        cookies[:tries] = cookies[:tries].to_i+1
+        if cookies[:tries].to_i == 1
+            @proximity == ""
+        elsif (cookies[:previousguess].to_i - secret).abs <= (@guess - secret).abs
+                @proximity = "Getting colder..."
+        elsif @guess == secret
+                @proximity = ""
         else
-                @proximity="Getting warmer..."
+                @proximity = "Getting warmer..."
         end
-        cookies[:previousguess]=@guess
+        cookies[:previousguess] = @guess
     end
 end
